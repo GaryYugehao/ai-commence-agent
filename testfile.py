94f14ffb112f7cf4cd51056d487513f6b41c38f2
Gehao Yu <g49yu@uawaterloo.ca>
2025-05-13 01:35:25 -0400

    Gemini API setup

--- testfile.py
+++ testfile.py
@@ -1,7 +1,14 @@
 from google import genai
 from google.genai import types
+import os
+from dotenv import load_dotenv
 
-client = genai.Client(api_key="YOUR_API_KEY")
+load_dotenv(dotenv_path='.idea/.env')
+gemini_key = os.getenv('GEMINI_API_KEY')
+if gemini_key is None:
+    print("API key not found. Please check the .env file.")
+
+client = genai.Client(api_key=gemini_key)
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
